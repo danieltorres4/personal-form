8a8fe75d8cbf5a721ba8b8db22450839b4d42594
danieltorres4 <dst990502@outlook.com>
2022-11-04 15:39:25 -0600

    Adding 2 new activities: splash and showInfor

--- app/src/main/java/com/outatime/personalform/ShowInfo.kt
+++ app/src/main/java/com/outatime/personalform/ShowInfo.kt
@@ -4,26 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.outatime.personalform.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class ShowInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_show_info)
     }
 
     fun click(view: View) {
-        val intent = Intent(this, ShowInfo::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
-
-    }
-
-    fun pickBirthday() {
-
     }
 }
