2a877f11a7d28c5a6374ac3fe7a2799ebb82e26f
danieltorres4 <dst990502@outlook.com>
2022-11-05 11:09:05 -0600

    Creating a new branch for testing

---
+++ app/src/main/java/com/outatime/personalform/ShowInfoTest.kt
@@ -0,0 +1,11 @@
+package com.outatime.personalform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowInfoTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_info_test)
+    }
+}
