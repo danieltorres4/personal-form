108550bc6cc08dd0f45fbc5cd80804fab69acde5
danieltorres4 <dst990502@outlook.com>
2022-11-06 11:21:12 -0600

    Solving parameters problem

--- app/src/main/java/com/outatime/personalform/ShowInfo.kt
+++ app/src/main/java/com/outatime/personalform/ShowInfo.kt
@@ -7,15 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import com.outatime.personalform.databinding.ActivityMainBinding
+import com.outatime.personalform.databinding.ActivityShowInfoBinding
 import kotlin.reflect.typeOf
 
 class ShowInfo : AppCompatActivity() {
 
-    private lateinit var binding2: ActivityMainBinding
+    private lateinit var binding2: ActivityShowInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding2 = ActivityMainBinding.inflate(layoutInflater)
+        binding2 = ActivityShowInfoBinding.inflate(layoutInflater)
         setContentView(binding2.root)
 
         val bundle = intent.extras
@@ -25,12 +26,15 @@ class ShowInfo : AppCompatActivity() {
         var accountNumber = bundle?.getString("accountNumber","")
         var email = bundle?.getString("email","")
 
+        binding2.tvShowName.text = name
+        binding2.tvShowBirthday.text = birthday
+        binding2.tvShowAn.text = accountNumber
+        binding2.tvShowEmail.text = email
 
-
-        Toast.makeText(this, "Name: $name", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Birthday: $birthday", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Account number: $accountNumber", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Email: $email", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "Name: $name", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "Birthday: $birthday", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "Account number: $accountNumber", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, "Email: $email", Toast.LENGTH_LONG).show()
 
     }
 
--- app/src/main/java/com/outatime/personalform/ShowInfoTest.kt
+++ app/src/main/java/com/outatime/personalform/ShowInfoTest.kt
@@ -2,10 +2,24 @@ package com.outatime.personalform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.outatime.personalform.databinding.ActivityMainBinding
 
 class ShowInfoTest : AppCompatActivity() {
+
+    private lateinit var testBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_info_test)
+        testBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(testBinding.root)
+
+        val bundle = intent.extras
+
+        var name = bundle?.getString("name","")
+        var birthday = bundle?.getString("birthday", "")
+        var accountNumber = bundle?.getString("accountNumber","")
+        var email = bundle?.getString("email","")
+
+
     }
 }
